fc7910a64521856ea7a9cf72e5101a9f06633b0a
SaenaKIM <tosk3927@gmail.com>
2022-03-28 20:42:11 +0900

    practice js - until function

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const player = {
+    name: "nico",
+    sayHello: function(otherPersionName) {
+        console.log("Hello, " + otherPersionName)
+    }
+}
+
+console.log(player);
+player.sayHello("nico");
+player.sayHello("nico");
+player.sayHello("nico");
+player.sayHello("nico");
+player.sayHello("nico");
+player.sayHello("nico");
